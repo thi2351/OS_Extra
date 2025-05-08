6238cd93bdf2127c507bf2aa05b146108cf92bb2
thi2351 <161857849+thi2351@users.noreply.github.com>
2025-05-08 12:18:38 +0700

    Update main.c
    Remove useless comment, change in main algorithm.

--- src/main.c
+++ src/main.c
@@ -113,7 +113,34 @@ void simulate_cfs(pcb_t *pcbs, int *arrival, int *remain, int num_cpu, int num_p
                 cfs_enqueue(start_ev.proc);
                 printf("Enqueue PID=%u\n", start_ev.proc->pid);
             }
-
+            //Step 2: Preempt some CPU that expired new timeslice:
+            for (int i = 0; i < num_cpu; i++) {
+                cpu_t *c = &cpu_m.cpu_list[i];
+                pcb_t *p = c->running_process;
+                if (!p) continue;
+                int idx = (int)(p - pcbs);
+                uint32_t new_timeslice = cfs_timeslice(p, cpu_m.total_weight_proc);
+                uint32_t old_timeslice = time_slice[idx];
+                uint32_t run_for = t - c->last_dispatch;
+                uint32_t remains = remain[idx];
+                event_t old_end = make_event(c, EVENT_END, p, c->last_dispatch + old_timeslice);
+                event_delete(&ev_t, &old_end);
+                if (run_for >= new_timeslice) {
+                    remain[idx] -= (int)run_for;
+                    cfs_task_tick(p, run_for, cpu_m.total_weight_proc);
+                    cpu_release(c, run_for);
+                    if (remain[p - pcbs] <= 0) {
+                        cfs_dequeue(p);
+                    }
+                    c->running_process = NULL;
+                    printf("Expired time-slice of PID=%u in CPU %u due to new process arrival\n", p->pid, c->cpu_id);
+                }
+                else {
+                    event_t new_end = make_event(c, EVENT_END, p, c->last_dispatch + new_timeslice); 
+                    event_tree_insert(&ev_t, &new_end);
+                    time_slice[idx] = new_timeslice;
+                }
+            }
             //Step 2: Try to assigned it to CPU
             while (1) {
                 cpu_t *c = cpu_peek();
@@ -126,7 +153,6 @@ void simulate_cfs(pcb_t *pcbs, int *arrival, int *remain, int num_cpu, int num_p
                 uint64_t slice = cfs_timeslice(p, cpu_m.total_weight_proc);
                 int    idx   = (int)(p - pcbs);
                 uint64_t run = slice < (uint64_t)remain[idx] ? slice : (uint64_t)remain[idx];
-            
                 event_t ev_end = make_event(c, EVENT_END, p, t + run);
                 event_tree_insert(&ev_t, &ev_end);
                 time_slice[idx] = (int)run;
@@ -248,219 +274,4 @@ int main(int argc, char *argv[]) {
 
 
 
-
-// while (done < num_process) {
-//     event_t ae;
-//     while (heap_peek(&arrivals, &ae) == 0 && ae.time <= t) {
-//         heap_pop(&arrivals, &ae);
-//         cfs_enqueue(ae.proc);
-//         printf("[t=%4llu] enqueue PID=%u\n", (unsigned long long)t, ae.proc->pid);
-//     }
-
-//     pcb_t *curr = cfs_pick_next();
-//     if (!curr) {
-//         if (heap_pop(&arrivals, &ae) == 0) {
-//             t = ae.time;
-//             cfs_enqueue(ae.proc);
-//             printf("[t=%4llu] enqueue PID=%u\n", (unsigned long long)t, ae.proc->pid);
-//             printf("\n");
-//             continue;
-//         }
-//         break;
-//     }
-
-//     int idx = (int)(curr - pcbs);
-//     assert(idx >= 0 && idx < num_process);
-
-//     uint64_t slice = cfs_timeslice(curr);
-//     uint64_t rem = remain[idx];
-//     uint64_t run_total = slice < rem ? slice : rem;
-//     uint64_t run_done = 0;
-
-//     event_t ae2;
-//     while (run_done < run_total) {
-//         uint64_t next_arr = UINT64_MAX;
-//         if (heap_peek(&arrivals, &ae2) == 0) next_arr = ae2.time;
-
-//         uint64_t t_until_arr = next_arr > t ? next_arr - t : 0;
-//         uint64_t to_run = run_total - run_done;
-//         if (t_until_arr > 0 && to_run > t_until_arr)
-//             to_run = t_until_arr;
-
-//         printf("[t=%4llu] run PID=%u for %4llu",
-//                (unsigned long long)t, curr->pid,
-//                (unsigned long long)to_run);
-//         printf("\n");
-
-//         t += to_run;
-//         run_done += to_run;
-
-//         // handle any arrivals exactly at new time t
-//         while (heap_peek(&arrivals, &ae2) == 0 && ae2.time == t) {
-//             heap_pop(&arrivals, &ae2);
-//             cfs_enqueue(ae2.proc);
-//             printf("[t=%4llu] enqueue PID=%u", (unsigned long long)t, ae2.proc->pid);
-//             printf("\n");
-//             run_total = cfs_timeslice(curr);
-//         }
-//         if (run_done >= run_total) {
-//             cfs_task_tick(curr, run_done);
-//             remain[idx] -= (int)run_done;
-//             continue;
-//         }
-//         // compare curr vs best (without updated vruntime for curr)
-//         pcb_t *best = cfs_pick_next();
-//         if (best != curr) {
-//             printf("[t=%4llu] preempt PID=%u -> PID=%u", (unsigned long long)t, curr->pid, best->pid);
-//             printf("\n");
-//             cfs_task_tick(curr, run_done);
-//             remain[idx] -= (int)run_done;
-
-//             curr = best;
-//             idx = (int)(curr - pcbs);
-//             slice = cfs_timeslice(curr);
-//             rem = remain[idx];
-//             run_total = slice < rem ? slice : rem;
-//             run_done = 0;
-//             break; 
-//         }
-//     }
-
-//     if (remain[idx] == 0) {
-//         cfs_dequeue(curr);
-//         finished[idx] = true;
-//         done++;
-//         printf("[t=%4llu] finish PID=%u\n", (unsigned long long)t, curr->pid);
-//     }
-// }
-
-// printf("All done at t=%llu\n", (unsigned long long)t);
-// heap_free(&arrivals);
-// free(finished);
-
-
-
-
-            // //Step 2: Recalculate time-slice
-            // for (int i = 0; i < num_cpu; i++) {
-            //     cpu_t *c = &cpu_m.cpu_list[i];
-            //     pcb_t *p = c->running_process;
-            //     if (!p) continue;
-            //     uint64_t run_done  = t - c->last_dispatch;
-            //     int      idx       = (int)(p - pcbs);
-            //     uint64_t slice     = cfs_timeslice(p, cpu_m.total_weight_proc);
-            //     uint64_t run_for   = slice < (uint64_t)remain[idx] ? slice : (uint64_t)remain[idx];
-            //     event_t old_ev = {
-            //         .ev   = EVENT_END,
-            //         .time = c->last_dispatch + run_for,
-            //         .proc = p,
-            //         .cpu  = c
-            //     };
-            //     event_delete(&ev_t, &old_ev);
-            //     if (run_done < run_for) {
-            //         uint64_t remaining = run_for - run_done;
-            //         event_t new_ev = {
-            //             .ev   = EVENT_END,
-            //             .time = t + remaining,
-            //             .proc = p,
-            //             .cpu  = c
-            //         };
-            //         event_tree_insert(&ev_t, &new_ev);
-            //         c->running_time = new_ev.time;
-            //     } else {
-            //         event_t new_ev = {
-            //             .ev   = EVENT_END,
-            //             .time = t,
-            //             .proc = p,
-            //             .cpu  = c
-            //         };
-            //         event_tree_insert(&ev_t, &new_ev);
-            //         c->running_time = t;
-            //     }
-            // }
-            // //Step 3: Release all CPU end time-slice
-            // event_t end_ev;
-            // while (event_tree_peek(&ev_t, &end_ev) 
-            //     && end_ev.ev == EVENT_END 
-            //     && end_ev.time == t) 
-            // {
-            //     event_tree_pop(&ev_t, &end_ev);
-            //     cpu_t   *c    = end_ev.cpu;
-            //     pcb_t   *p    = end_ev.proc;
-            //     int       idx = (int)(p - pcbs);
-            //     uint64_t  run_done = t - c->last_dispatch;
-
-            //     remain[idx] -= (int)run_done;
-            //     cfs_task_tick(p, run_done);
-            //     cpu_release(c, run_done);
-
-            //     if (remain[idx] == 0) {
-            //         done++;
-            //         cfs_dequeue(p);
-            //     }
-            // }
-            // //Step 4: Enqueue new process into CPU
-            // while (1) {
-            //     cpu_t *idle = cpu_peek();
-            //     pcb_t *next = cfs_pick_next();
-            //     if (!idle || !next) break;
-        
-            //     cpu_dispatch(next, t);
-        
-            //     int idx2 = (int)(next - pcbs);
-            //     uint64_t slice2   = cfs_timeslice(next, cpu_m.total_weight_proc);
-            //     uint64_t run_for2 = slice2 < (uint64_t)remain[idx2]
-            //                         ? slice2
-            //                         : (uint64_t)remain[idx2];
-        
-            //     event_t ne = {
-            //         .ev   = EVENT_END,
-            //         .time = t + run_for2,
-            //         .proc = next,
-            //         .cpu  = idle
-            //     };
-            //     event_tree_insert(&ev_t, &ne);
-        
-            //     idle->last_dispatch  = t;
-            // }
-            // //Step 5: Maximum: The new process need to enter the CPU
-            // int best_idx = -1;
-            // double best = -1;
-            // pcb_t* next = cfs_pick_next();
-            // if (next->vruntime != 0) continue;
-            // for (int i = 0; i < num_cpu; i++) {
-            //     if (cpu_m.cpu_list[i].running_process != NULL && t - cpu_m.cpu_list[i].last_dispatch >= MIN_GRANULARITY_NSEC && 
-            //         cpu_m.cpu_list[i].running_process->vruntime >= best) {
-            //         best_idx = i;
-            //     }
-            // }
-            // //If not found, prepare for the first process reach and the biggest runtime
-            // best = -1;
-            // if (best_idx == -1) {
-            //     for (int i = 0; i < num_cpu; i++) {
-            //         if (cpu_m.cpu_list[i].running_process != NULL &&
-            //             t - cpu_m.cpu_list[i].last_dispatch >= best) {
-
-            //             if (t - cpu_m.cpu_list[i].last_dispatch == best) {
-            //                 best_idx = cpu_m.cpu_list[i].running_process->vruntime > cpu_m.cpu_list[best_idx].running_process->vruntime 
-            //                 ? i : best_idx;
-            //             }
-            //             best = t - cpu_m.cpu_list[i].last_dispatch;
-            //             best_idx = i;
-            //         }
-            //     }
-            //     cfs_dequeue(ev.proc);
-            //     event_t key3 = {
-            //         .cpu = NULL,
-            //         .ev = EVENT_ARRIVAL,
-            //         .proc = ev.proc,
-            //         .time = t + MIN_GRANULARITY_NSEC
-            //     };
-            //     event_t key4 = {
-            //         .cpu = &cpu_m.cpu_list[i];
-            //         .proc = 
-            //     }
-            //     event_tree_insert(&ev_t, &key3);
-            // }
-            // //Preempt the process with maximum vruntime in list cpu
             
